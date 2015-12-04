99f0886a20563df857ba0cddd76448eb49a7306b
bcabanes <benjamin.cabanes@ugroupmedia.com>
2015-12-04 12:21:34 -0500

    Update takePictures module

--- www/app/modules/takePictures/takePicturesController.js
+++ www/app/modules/takePictures/takePicturesController.js
@@ -3,9 +3,31 @@
 
   angular
     .module('bc-takePictures.coreController', [])
-    .controller('takePicturesController', function() {
+    .controller('takePicturesController', ['$cordovaCamera', function($cordovaCamera) {
+        var _cameraSettings = {
+          quality: 100,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 500,
+          targetHeight: 500,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+          correctOrientation: false
+        };
+
       this.takePicture = function() {
-console.log('takingPicture');
+        $cordovaCamera
+          .getPicture(_cameraSettings)
+          .then(function(imageData) {
+            var image = document.getElementById('myPicture');
+            image.src = "data:image/jpeg;base64," + imageData;
+          })
+          .catch(function(error) {
+            // Handling the error...
+            console.error(error);
+          });
       };
-    });
+    }]);
 })(angular);
