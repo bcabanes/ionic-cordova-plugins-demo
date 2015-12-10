1131912e188c6986c386352e353e5582dae87186
bcabanes <benjamin.cabanes@ugroupmedia.com>
2015-12-10 11:33:19 -0500

    Cleaning custom player module

--- www/app/modules/customPlayers/customPlayersController.js
+++ www/app/modules/customPlayers/customPlayersController.js
@@ -12,8 +12,8 @@
     .controller('customPlayersController', ['$cordovaFile', '$sce', '$timeout', function($cordovaFile, $sce, $timeout) {
       var self = this;
       this.playerApi = null;
-      // this.fileUrl = 'http://benjamincabanes.com/medias/tests/wanderers.mp4';
-      this.filePath = cordova.file.externalRootDirectory+'Downloads/';
+      this.fileUrl = 'http://benjamincabanes.com/medias/tests/wanderers.mp4';
+      // this.filePath = cordova.file.externalRootDirectory+'Downloads/';
       this.fileName = 'myVideo.mp4';
 
       this.config = {
@@ -23,11 +23,12 @@
         nativeControls: false,
         isLive: true,
 				sources: [
-					{src: $sce.trustAsResourceUrl(self.filePath+self.fileName), type: 'video/mp4'}
+					// {src: $sce.trustAsResourceUrl(self.filePath+self.fileName), type: 'video/mp4'}
+					{src: $sce.trustAsResourceUrl(self.fileUrl), type: 'video/mp4'}
 				],
         theme: 'vendors/videogular-themes-default/videogular.css'
       };
-console.log(this.config.sources);
+
       this.onPlayerReady = function(api) {
         this.playerApi = api;
       };
