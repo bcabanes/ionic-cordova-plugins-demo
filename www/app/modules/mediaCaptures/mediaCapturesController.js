0cf26de5418ea4c28ecc95d1a977518a1e3a47e0
bcabanes <benjamin.cabanes@ugroupmedia.com>
2015-12-15 10:44:40 -0500

    Update media capture module

--- www/app/modules/mediaCaptures/mediaCapturesController.js
+++ www/app/modules/mediaCaptures/mediaCapturesController.js
@@ -23,13 +23,29 @@ console.log(videoData);
        * Don't forget to have the right permission in the AndroidManifest.xml:
        * <uses-permission android:name="android.permission.CAMERA" />
        */
-      var video = document.querySelector('video');
+      this.videoElement = document.querySelector('video');
+      this.streamRecorder = null;
+      this.webcamStream = null;
       navigator.webkitGetUserMedia({video: true}, function(stream) {
-        video.src = window.URL.createObjectURL(stream);
-        localMediaStream = stream;
+        self.videoElement.src = window.URL.createObjectURL(stream);
+        self.webcamStream = stream;
       }, function(error) {
 console.log(error);
       });
+
+      this.startRecording = function() {
+        if (this.streamRecorder) {
+          return this.stopRecording();
+        }
+console.log(this.webcamStream);
+        this.streamRecorder = this.webcamStream.start();
+      };
+
+      this.stopRecording = function() {
+        this.streamRecorder.getRecordedData(function(videoBlob) {
+          console.log(videoBlob);
+        });
+      };
 
     }]);
 })(angular);
